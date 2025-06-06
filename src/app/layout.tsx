d59624716e6e2112c70fc3e4b11d1a6f779b427b
trungenglish <nguyentrung23232323@gmail.com>
2025-06-06 23:39:13 +0700

    feat: add header and footer finish #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@components/Header";
 import './globals.css'
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
     children,
@@ -11,6 +12,7 @@ export default function RootLayout({
             <body className="antialiased">
                 <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     );
